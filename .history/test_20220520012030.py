de12d1a5e59fab5dc741fb16a25242893ee1a771
Seungwoo, Joo <83846985+thisIsJooS@users.noreply.github.com>
2022-05-20 21:58:45 +0900

    Delete .history directory

--- .history/test_20220520012030.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome('./chromedriver')
-
-driver.get('https://www.naver.com')
--- .history/test_20220520012310.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome('./chromedriver')
-
-def open():
-    driver.get('https://www.naver.com')
